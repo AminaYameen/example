123fea61af13cf37cd13b33b31bdbac26b245583
AminaYameen <aminaarooj1999@gmail.com>
2024-11-09 03:12:09 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+  <>
+  <div>
+    <h1>Login In</h1>
+    <input type="text"/> <br></br>
+    <input type="text"/> <br></br>
+    
+    <button type="submit"> Submit</button>
+  </div>
+    </>
+  )
+}
+
+export default App
